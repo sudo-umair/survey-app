2040c720783901d9d4aaee15f14bf43a6ad4920c
Muhammad Umair <muhammadumair523@gmail.com>
2023-05-20 00:00:59 +0500

    path aliases setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './src',
+            '@assets': './src/assets',
+            '@common': './src/common',
+            '@config': './src/config',
+            '@components': './src/components',
+            '@helpers': './src/helpers',
+            '@hooks': './src/hooks',
+            '@navigation': './src/navigation',
+            '@services': './src/services',
+            '@redux': './src/redux',
+            '@screens': './src/screens',
+            '@types': './src/types',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
---
+++ metro.config.js
@@ -0,0 +1,28 @@
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== 'svg'), // Add any other asset extensions you may use
+      sourceExts: [...sourceExts, 'jsx', 'js', 'ts', 'tsx'], // Add any other source extensions you may use
+      extraNodeModules: {
+        '@': `${__dirname}/src`,
+        '@assets': `${__dirname}/src/assets`,
+        '@common': `${__dirname}/src/common`,
+        '@config': `${__dirname}/src/config`,
+        '@components': `${__dirname}/src/components`,
+        '@helpers': `${__dirname}/src/helpers`,
+        '@hooks': `${__dirname}/src/hooks`,
+        '@navigation': `${__dirname}/src/navigation`,
+        '@services': `${__dirname}/src/services`,
+        '@redux': `${__dirname}/src/redux`,
+        '@screens': `${__dirname}/src/screens`,
+        '@types': `${__dirname}/src/types`,
+        '@utils': `${__dirname}/src/utils`,
+      },
+    },
+  };
+})();
