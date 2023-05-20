62c65e67c4a280a165bcd8380e4e10048100e918
Muhammad Umair <muhammadumair523@gmail.com>
2023-05-21 02:23:49 +0500

    minor text fix

---
+++ src/screens/enumerator/section-a-screen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const SectionAScreen = () => {
+  return (
+    <View>
+      <Text>SectionAScreen</Text>
+    </View>
+  );
+};
+
+export default SectionAScreen;
+
+const styles = StyleSheet.create({});
