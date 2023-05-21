fb6416d9b26da9cfac24668060b4141a00ebd807
Muhammad Umair <muhammadumair523@gmail.com>
2023-05-21 20:35:01 +0500

    metro config

--- metro.config.js
+++ metro.config.js
@@ -1,27 +1,4 @@
-const { getDefaultConfig } = require('metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'), // Add any other asset extensions you may use
-      sourceExts: [...sourceExts, 'jsx', 'js', 'ts', 'tsx'], // Add any other source extensions you may use
-      extraNodeModules: {
-        '@assets': `${__dirname}/src/assets`,
-        '@common': `${__dirname}/src/common`,
-        '@config': `${__dirname}/src/config`,
-        '@components': `${__dirname}/src/components`,
-        '@helpers': `${__dirname}/src/helpers`,
-        '@hooks': `${__dirname}/src/hooks`,
-        '@interfaces': `${__dirname}/src/interfaces`,
-        '@navigation': `${__dirname}/src/navigation`,
-        '@services': `${__dirname}/src/services`,
-        '@redux': `${__dirname}/src/redux`,
-        '@screens': `${__dirname}/src/screens`,
-        '@utils': `${__dirname}/src/utils`,
-      },
-    },
-  };
-})();
+module.exports = getDefaultConfig(__dirname);
