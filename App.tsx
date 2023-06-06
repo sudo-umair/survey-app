fd16d160e02b602185a03af350cf4d3ef19de05d
Muhammad Umair <muhammadumair523@gmail.com>
2023-06-06 12:45:51 +0500

    minor updates

--- App.tsx
+++ App.tsx
@@ -45,6 +45,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.BACKGROUND,
+    paddingVertical: Platform.OS === 'android' ? 0 : 25,
   },
 });
