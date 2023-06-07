86527abca773ca402d4649dd7e41c9d55aa25bc0
Muhammad Umair <muhammadumair523@gmail.com>
2023-06-07 16:26:43 +0500

    ui updates

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import AppNavigator from '@navigation/app-navigator';
 import { store, persistor } from '@redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import { COLORS } from '@common/colors';
 import { useNetInfo } from '@react-native-community/netinfo';
@@ -22,23 +22,25 @@ export default function App() {
   }, [isInternetReachable]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <AppNavigator />
-        </PersistGate>
-      </Provider>
-      <StatusBar
-        backgroundColor={COLORS.PRIMARY}
-        style={Platform.OS === 'ios' ? 'dark' : 'light'}
-      />
-      <Toast
-        position='bottom'
-        visibilityTime={2000}
-        keyboardOffset={10}
-        bottomOffset={10}
-      />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <AppNavigator />
+          </PersistGate>
+        </Provider>
+        <StatusBar
+          backgroundColor={COLORS.PRIMARY}
+          style={Platform.OS === 'ios' ? 'dark' : 'light'}
+        />
+        <Toast
+          position='bottom'
+          visibilityTime={2000}
+          keyboardOffset={10}
+          bottomOffset={10}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -46,6 +48,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.BACKGROUND,
-    paddingVertical: Platform.OS === 'android' ? 0 : 25,
+    // paddingVertical: Platform.OS === 'android' ? 0 : 25,
   },
 });
